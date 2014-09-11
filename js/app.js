4d9711cb0c4cdcaceb389f3419c52528a6b36a5a
Marcin Gajewski <vegijtha@gmail.com>
2014-09-11 12:54:49 +0200

    bootstrap grids

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ App = Ember.Application.create({
  /*ROUTES*/
 App.Router.map(function() {
   this.resource("arts");
-	this.resource('art', { path: '/products/:title' });
+	this.resource('art', { path: '/art/:title' });
 	this.route("photo");
 	this.route("design");
 	this.route("about");
@@ -30,21 +30,41 @@ App.ArtsRoute = Ember.Route.extend({
 });
 App.ArtRoute = Ember.Route.extend({
   model: function(params) {
-    return App.ART.findBy('title', params.title); 
+    return App.ART.findBy('title', params.title);
   }
 });
 
 
 /*DATA*/
 App.ART = [
-  {
+  {	id:1,
     title: 'Adin',
     description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
     image: 'images/min.png'
   },
-  {
+  { id:2,
 		title: 'Dwa',
 		description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+		image: 'images/min2.png'
+	},
+	{	id:3,
+		title: 'Adin',
+		description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
 		image: 'images/min.png'
-	}
+	},
+	{	id:4,
+		title: 'Adin',
+		description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+		image: 'images/min.png'
+	},
+	{	id:5,
+		title: 'Adin',
+		description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+		image: 'images/min.png'
+	},
+	{	id:6,
+		title: 'Adin',
+		description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+		image: 'images/min.png'
+	},
 ];
